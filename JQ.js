f91b9e9edc35f0f67604449d57bec7a422777c2b
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-16 21:08:06 +0700

    change_style_css_id

--- JQ.js
+++ JQ.js
@@ -1,4 +1,6 @@
 function func1()
 {
-    $("div").css('background-color','orange');
+    $("#p1").css('font-style', 'italic');
+    /*trong truong hop co 2 id trung ten, vd This is paragraph 1 co id p1 va This is paragraph 3 cung co id p1 thi jquery chi thay doi style 
+    cua This is paragraph 1, noi cach khac id dung cho jquery la unique id*/ 
 }
