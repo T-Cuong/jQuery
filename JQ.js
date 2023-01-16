ad3dce0cd5690880ef536fb7b7c569a07b972f88
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-16 21:10:27 +0700

    change_css_style_class

--- JQ.js
+++ JQ.js
@@ -1,6 +1,7 @@
 function func1()
 {
-    $("#p1").css('font-style', 'italic');
+    $(".p1").css('font-style', 'italic');
     /*trong truong hop co 2 id trung ten, vd This is paragraph 1 co id p1 va This is paragraph 3 cung co id p1 thi jquery chi thay doi style 
-    cua This is paragraph 1, noi cach khac id dung cho jquery la unique id*/ 
+    cua This is paragraph 1, noi cach khac id dung cho jquery la unique id
+    trong truong hop muon thay doi ca 2 nhu o tren ta thay id bang class*/ 
 }
