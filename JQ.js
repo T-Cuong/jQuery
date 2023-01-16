1c93c2f581af6f27e053aa92d9e0e80038967536
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-16 22:24:25 +0700

    change_style_class_id_li

--- JQ.js
+++ JQ.js
@@ -1,7 +1,5 @@
 function func1()
 {
-    $(".p1").css('font-style', 'italic');
-    /*trong truong hop co 2 id trung ten, vd This is paragraph 1 co id p1 va This is paragraph 3 cung co id p1 thi jquery chi thay doi style 
-    cua This is paragraph 1, noi cach khac id dung cho jquery la unique id
-    trong truong hop muon thay doi ca 2 nhu o tren ta thay id bang class*/ 
+    $("#div1,#div2").css('background-color','green');
+    $("li").css('background-color','purple')
 }
