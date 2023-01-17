741e395f50703e857fbfb022e7cef67544dfa119
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-17 09:51:01 +0700

    jquery_event

--- JQ.js
+++ JQ.js
@@ -1,7 +1,41 @@
-function func1()
-{
-    //$("div > p").fadeToggle();
-    //$("p:first").fadeToggle();//chi change dc phan tu thu nhat thoi cac phan tu thu second third thi ko dc
-    //$("li:even").fadeToggle();//an li le
-    //$("li:odd").fadeToggle();//an li chang
-}
+// $("button").click(func1);
+// function func1(){
+//     $("#img1").css('width','1000px');
+// }
+
+// $("button").click(function(){
+//     $("#img1").css('width','500px');
+// })
+
+//Use ready() to make a function available after the document is loaded:
+// $("document").ready(function() {
+//     $("button").click(function(){
+//         $("#img1").css('width','500px');
+//     })
+// })
+
+//double click
+// $("document").ready(function() {
+//     $("button").dblclick(function(){
+//         $("#img1").css('width','500px');
+//     })
+// })
+
+//mouse enter & mouse leave
+$("document").ready(function(){
+    // $("#img1").mouseenter(function(){
+    //     $("#img1").css('width','500px');
+    // });
+    // $("#img1").mouseleave(function(){
+    //     $("#img1").css('width','250px');
+    // });  
+    $("#img1").hover(func1, func2)
+    function func1()
+    {
+        $("#img1").css('width','500px');
+    }
+    function func2()
+    {
+        $("#img1").css('width','250px')
+    }
+})
