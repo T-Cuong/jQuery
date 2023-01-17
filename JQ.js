6797f3739ff6ce9266eef32d330ec91ed2d971ba
T-Cuong <trongcuong17pfiev@gmail.com>
2023-01-17 10:41:21 +0700

    jquery_effect

--- JQ.js
+++ JQ.js
@@ -1,41 +1,35 @@
-// $("button").click(func1);
-// function func1(){
-//     $("#img1").css('width','1000px');
-// }
-
-// $("button").click(function(){
-//     $("#img1").css('width','500px');
-// })
-
-//Use ready() to make a function available after the document is loaded:
-// $("document").ready(function() {
-//     $("button").click(function(){
-//         $("#img1").css('width','500px');
-//     })
-// })
-
-//double click
-// $("document").ready(function() {
-//     $("button").dblclick(function(){
-//         $("#img1").css('width','500px');
-//     })
-// })
-
-//mouse enter & mouse leave
 $("document").ready(function(){
-    // $("#img1").mouseenter(function(){
-    //     $("#img1").css('width','500px');
-    // });
-    // $("#img1").mouseleave(function(){
-    //     $("#img1").css('width','250px');
-    // });  
-    $("#img1").hover(func1, func2)
-    function func1()
-    {
-        $("#img1").css('width','500px');
-    }
-    function func2()
-    {
-        $("#img1").css('width','250px')
-    }
+    $("#btn1").click(function(){
+        $("#img1").hide(2000);
+    })
+    $("#btn2").click(function(){
+        $("#img1").show(2000);
+    })
+    $("#btn3").click(function(){
+        $("#img1").toggle(2000);
+    })
+
+    $("#btn4").click(function(){
+        $("#img1").fadeIn(2000);
+    })
+    $("#btn5").click(function(){
+        $("#img1").fadeOut(2000);
+    })
+    $("#btn6").click(function(){
+        $("#img1").fadeToggle(2000);
+    })
+
+    $("#btn7").click(function(){
+        $("#img1").slideUp(2000);
+    })
+    $("#btn8").click(function(){
+        $("#img1").slideDown(2000);
+    })
+    $("#btn9").click(function(){
+        $("#img1").slideToggle();
+    })
+
+    $("#btn10").click(function(){
+        $("#img1").stop();
+    })
 })
